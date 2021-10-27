8829719ffbfe4b8ee56a558bbe0d38ecb49f31d5
xizher <xizher@163.com>
2021-10-27 19:36:53 +0800

    chore: 移除一级模块单独打包方式

--- transfor/index.cjs.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 首字母变小写
- * @param str 字符串
- */
-declare function toLowerCaseFirstIndex(str: string): string;
-
-export { toLowerCaseFirstIndex };
--- transfor/index.esm.js
+++
@@ -1,9 +0,0 @@
-/**
- * 首字母变小写
- * @param str 字符串
- */
-function toLowerCaseFirstIndex(str) {
-    return str.replace(str[0], str[0].toLowerCase());
-}
-
-export { toLowerCaseFirstIndex };
